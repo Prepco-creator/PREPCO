8e0fe7f528a4f057da7f1e8efa68722ef459c90b
Akram-00 <aliakram9789@gmail.com>
2024-12-31 17:18:23 +0530

    overflowproblem, health package bg colors , hover effects , hero animation correction

--- src/components/about/Card.tsx
+++ src/components/about/Card.tsx
@@ -5,7 +5,7 @@ import Image from 'next/image'
 
 const Card: React.FC<CardProps> = ({ title, icon, content, className, hClassName }) => {
   return (
-    <section className={`${className} rounded-3xl flex flex-col items-center justify-center gap-4 lg:gap-8 p-7 text-center bg-custom-gradient shadow-double-inset text-primaryLight`}>
+    <section className={`${className} rounded-3xl flex flex-col items-center justify-center gap-4 lg:gap-8 p-7 text-center bg-custom-gradient shadow-double-inset text-primaryLight hover:scale-105 transition-all duration-75 ease-in-out`}>
       <Image unoptimized width={84} height={84} src={icon} alt={title} />
       <header className={hClassName ? hClassName : 'text-custom-16-bold lg:text-custom-24'}>{title}</header>
       <p className='text-custom-14 lg:text-custom-20-2'>{content}f</p>
