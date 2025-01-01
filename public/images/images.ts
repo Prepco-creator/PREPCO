1723c3c325ce6dca4655e9b0251ab1250ad63506
Akram-00 <aliakram9789@gmail.com>
2025-01-02 03:43:09 +0530

    Images are changed for plans section

--- public/images/images.ts
+++ public/images/images.ts
@@ -1,5 +1,4 @@
 // GIF
-
 import drink from './gifs/drink.gif';
 import exercise from './gifs/exercise.gif';
 import lunchBox from './gifs/lunch-box.gif';
@@ -11,57 +10,55 @@ import family from './gifs/family.gif';
 import meditation from './gifs/meditation.gif';
 import reading from './gifs/reading.gif';
 import walk from './gifs/walk.gif';
-import hollowCircles from './gifs/hollow-circles.gif'
-import bulb from './gifs/bulb.gif'
-import tabletGuy from './gifs/tablet-guy.gif'
-import innovationBulb from './gifs/innovation-bulb.gif'
-import threeGuys from './gifs/three-guys.gif'
-import twoGuys from './gifs/two-guys.gif'
-import yogaUpDown from './gifs/yoga-up-down.gif'
-import runningWoman from './gifs/running-woman.gif'
-import lovingFamily from './gifs/loving-family.gif'
-import coupleWalking from './gifs/couple-walking.gif'
-import drinkWater from './gifs/drink-water.gif'
+import hollowCircles from './gifs/hollow-circles.gif';
+import bulb from './gifs/bulb.gif';
+import tabletGuy from './gifs/tablet-guy.gif';
+import innovationBulb from './gifs/innovation-bulb.gif';
+import threeGuys from './gifs/three-guys.gif';
+import twoGuys from './gifs/two-guys.gif';
+import yogaUpDown from './gifs/yoga-up-down.gif';
+import runningWoman from './gifs/running-woman.gif';
+import lovingFamily from './gifs/loving-family.gif';
+import coupleWalking from './gifs/couple-walking.gif';
+import drinkWater from './gifs/drink-water.gif';
 import oneMemberPlan from './gifs/one-member.gif';
-import threeMemberPlan from './gifs/three-members-plan.gif'
+import threeMemberPlan from './gifs/three-members-plan.gif';
+
 // SVG
-
-import logo from './svg/Logo.svg'
-import doubleQuote from './svg/Quotes.svg'
-import logoIcon from './svg/LogoIcon.svg'
-import peopleDuo from './svg/peopleduo.svg'
-import dummyHero from './svg/dummyHeroImage.svg'
-import LogoWhite from './svg/LogoWhite.svg'
-import whiteLogoIcon from './svg/whiteLogoIcon.svg'
-import xmark from './svg/xmark.svg'
-
+import logo from './svg/Logo.svg';
+import doubleQuote from './svg/Quotes.svg';
+import logoIcon from './svg/LogoIcon.svg';
+import peopleDuo from './svg/peopleduo.svg';
+import dummyHero from './svg/dummyHeroImage.svg';
+import LogoWhite from './svg/LogoWhite.svg';
+import whiteLogoIcon from './svg/whiteLogoIcon.svg';
+import xmark from './svg/xmark.svg';
+import doctorConsultation from './svg/doctor-consultaion.svg';
+import microscope from './svg/microscope.svg';
+import virtualConsultation from './svg/virtual-consultation.svg';
+import dentalInsurance from './svg/dental-inusrance.svg';
+import heartBeatCheck from './svg/heart-beat-check.svg';
+import offer from './svg/offer.svg';
+import dietian from './svg/dietian.svg';
+import medicineTablet from './svg/medicine-tablet.svg';
+import physicalTherapy from './svg/physical-therapy.svg';
+import zumbaSession from './svg/zumba-session.svg';
 
 // JPG
-import testimonialImage from './jpgs/indian_testimonial_1.jpg'
-import testimonialImage2 from './jpgs/indian_testimonial_2.jpg'
+import testimonialImage from './jpgs/indian_testimonial_1.jpg';
+import testimonialImage2 from './jpgs/indian_testimonial_2.jpg';
 
-//PNG
-import aboutHero from './pngs/about-hero.png'
-import walkingCouple from './pngs/walking-couple.png'
-import doctorConsultation from './pngs/doctor-consultaion.png';
-import microscope from './pngs/microscope.png';
-import virtualConsultation from './pngs/virtual-consultation.png';
-import dentalInsurance from './pngs/dental-inusrance.png';
-import heartBeatCheck from './pngs/heart-beat-check.png';
-import offer from './pngs/offer.png';
-import dietian from './pngs/dietian.png';
-import medicineTablet from './pngs/medicine-tablet.png';
-import physicalTherapy from './pngs/physical-therapy.png';
-import zumbaSession from './pngs/zumba-session.png';
-import youtubeIcon from './pngs/youtube-icon.png'
-import whatsappIcon from './pngs/whatsapp-icon.png'
-import twitterIcon from './pngs/twitter-icon.png'
-import instagramIcon from './pngs/instagram-icon.png'
-import phoneIcon from './pngs/phone.png'
-import mailIcon from './pngs/mail.png'
-import popupImage from './pngs/popup-1.png'
-import pattern from './pngs/pattern-removebg-preview.png'
-
+// PNG
+import aboutHero from './pngs/about-hero.png';
+import walkingCouple from './pngs/walking-couple.png';
+import youtubeIcon from './pngs/youtube-icon.png';
+import whatsappIcon from './pngs/whatsapp-icon.png';
+import twitterIcon from './pngs/twitter-icon.png';
+import instagramIcon from './pngs/instagram-icon.png';
+import phoneIcon from './pngs/phone.png';
+import mailIcon from './pngs/mail.png';
+import popupImage from './pngs/popup-1.png';
+import pattern from './pngs/pattern-removebg-preview.png';
 
 
 const gifs = {
