b365a7f540f401732a5fa2266919785bf2de4ab4
Akram-00 <aliakram9789@gmail.com>
2025-01-03 18:40:54 +0530

    Flipping fo cards are done for prepco cards

--- src/components/plan/HealthPackage.tsx
+++ src/components/plan/HealthPackage.tsx
@@ -6,20 +6,42 @@ const HealthPackage: React.FC<{ healthPackage: HealthPackageDetails }> = ({
   healthPackage,
 }) => {
   return (
-    <div className="p-5 overflow-hidden bg-primary1 shadow-double-inset rounded-[30px] flex flex-col items-center justify-center gap-2 hover:transform hover:-translate-y-2 transition-all">
-      <Image
-        src={healthPackage.iconSrc}
-        alt={healthPackage.id}
-        width={60}
-        height={60}
-        className="stroke-white"
-      />
-      <h5 className="text-custom-16-bold lg:text-custom-20 lg:font-extrabold text-white">
-        {healthPackage.title}
-      </h5>
-      <p className="text-custom-14 lg:text-custom-16 text-white">
-        {healthPackage.description}
-      </p>
+    <div className="flip-card">
+      <div className="flip-card-inner">
+        {/* Front Face */}
+        <div className="flip-card-front p-5 bg-primary1 shadow-double-inset rounded-[30px] flex flex-col items-center justify-center gap-2 card-rotation">
+          <Image
+            src={healthPackage.iconSrc}
+            alt={healthPackage.id}
+            width={60}
+            height={60}
+            className="stroke-white"
+          />
+          <h5 className="text-custom-16-bold lg:text-custom-20 lg:font-extrabold text-white">
+            {healthPackage.title}
+          </h5>
+          <p className="text-custom-14 lg:text-custom-16 text-white">
+            {healthPackage.description}
+          </p>
+        </div>
+
+        {/* Back Face */}
+        <div className="flip-card-back p-5 bg-primary1 shadow-double-inset rounded-[30px] flex flex-col items-center justify-center gap-2">
+          <Image
+            src={healthPackage.iconSrc}
+            alt={healthPackage.id}
+            width={60}
+            height={60}
+            className="stroke-white"
+          />
+          <h5 className="text-custom-16-bold lg:text-custom-20 lg:font-extrabold text-white">
+            {healthPackage.title}
+          </h5>
+          <p className="text-custom-14 lg:text-custom-16 text-white">
+            {healthPackage.description}
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
