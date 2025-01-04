177d02e02f6e055a1e9e3a9d5d59ca68bc76dde2
Akram-00 <aliakram9789@gmail.com>
2025-01-04 12:35:50 +0530

    Features Plan Page design 3

--- src/components/plan/HealthPackage.tsx
+++ src/components/plan/HealthPackage.tsx
@@ -8,28 +8,30 @@ const HealthPackage: React.FC<{ healthPackage: HealthPackageDetails }> = ({
   return (
     <div className="relative group min-h-[271px]">
       {/* Stacked Card (background card) */}
-      <div className="absolute inset-0 -z-10 translate-y-2 bg-secondary rounded-[4px] w-11/12 mx-auto"></div>
+      {/* <div className="absolute inset-0 z-10 translate-y-3 bg-primary rounded-[4px] w-11/12 mx-auto"></div> */}
 
       {/* Main Card */}
-      <div className="p-5 h-full bg-custom-gradient shadow-md rounded-[4px] flex flex-col items-center justify-center gap-2 transition-all">
+      <div className="p-5 z-[11] relative h-full bg-white shadow-md rounded-[4px] flex flex-col items-center justify-center gap-2 transition-all group-hover:bg-primary group-hover:text-white">
         {/* Icon */}
         <div className="relative transition-transform group-hover:-translate-y-2">
-          <Image
-            src={healthPackage.iconSrc}
-            alt={healthPackage.id}
-            width={60}
-            height={60}
-            className="stroke-white"
-          />
+          <div className="flex items-center justify-center w-16 h-16 rounded-full bg-transparent group-hover:bg-white transition-all">
+            <Image
+              src={healthPackage.iconSrc}
+              alt={healthPackage.id}
+              width={40}
+              height={40}
+              className="transition-all"
+            />
+          </div>
         </div>
 
         {/* Title */}
-        <h5 className="text-custom-16-bold lg:text-custom-20 lg:font-extrabold text-white">
+        <h5 className="text-custom-16-bold lg:text-custom-20 lg:font-extrabold transition-all group-hover:text-white">
           {healthPackage.title}
         </h5>
 
         {/* Description */}
-        <p className="text-custom-14 lg:text-custom-16 text-white">
+        <p className="text-custom-14 lg:text-custom-16 transition-all group-hover:text-white">
           {healthPackage.description}
         </p>
       </div>
@@ -38,3 +40,4 @@ const HealthPackage: React.FC<{ healthPackage: HealthPackageDetails }> = ({
 };
 
 export default HealthPackage;
+
--- src/components/plan/PlanDetail.tsx
+++ src/components/plan/PlanDetail.tsx
@@ -4,7 +4,7 @@ import HealthPackagesGrid from './HealthPackagesGrid'
 
 const PlanDetail: React.FC<PlanDetailProps> = ({ title, bigDescription, healthPackages }) => {
     return (
-        <section className='flex flex-col gap-6 lg:gap-8 text-center p-4 lg:p-16'>
+        <section className='flex flex-col gap-6 lg:gap-8 text-center p-4 lg:p-16 bg-accent-2'>
             <h1 className='capitalize text-custom-20-bold lg:text-custom-40 text-primary'>Enhanced {title} for everyone</h1>
             <p className='text-left lg:text-center text-custom-14 lg:text-custom-20-2 text-secondaryDark'>{bigDescription}</p>
             <HealthPackagesGrid healthPackages={healthPackages} />
