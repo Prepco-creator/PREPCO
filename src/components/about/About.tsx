43fac0edc46135c8d866af21dde8b99c653c47d5
Akram-00 <aliakram9789@gmail.com>
2025-01-20 19:23:31 +0530

    Prepco Double Video Multilingual is updated

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -5,6 +5,7 @@ import MissionAndVision from './MissionAndVision'
 import WhyChoosePrepco from './WhyChoosePrepco'
 import AboutCTA from './AboutCTA'
 import Footer from '../comman/Footer'
+import { videos } from '../../../public/assets'
 
 const About = () => {
     return (
@@ -12,9 +13,22 @@ const About = () => {
             <Banner title='About' />
             <HeroSection />
             <MissionAndVision />
+            <AboutCTA
+                description="From fitness guides that keep you moving to wellness plans that work for you, Prepco is your
+partner in living your healthiest, happiest life."
+                title="Our Story"
+                tamilVideo={videos.hero}
+                englishVideo={videos.hero2}
+            />
             <WhyChoosePrepco />
-            <AboutCTA />
-            <Footer/>
+            <AboutCTA
+                description=" From fitness guides that keep you moving to wellness plans that work for you, Prepco is your partner in living your healthiest, happiest life."
+                title="Let Prepco show you how easy taking care of yourself can really be. Because your health deserves the best—and so do you."
+                tamilVideo={videos.hero}
+                englishVideo={videos.hero2}
+            />
+
+            <Footer />
         </>
         // our mission & vision
         // why choose prepco
--- src/components/about/AboutCTA.tsx
+++ src/components/about/AboutCTA.tsx
@@ -1,21 +1,24 @@
 "use client";
-import React, { useEffect } from "react";
-import { videos } from "../../../public/assets";
+import React, { useEffect, useState } from "react";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 
-// Register ScrollTrigger plugin
 gsap.registerPlugin(ScrollTrigger);
 
-const AboutCTA = () => {
+interface AboutCTAProps {
+  title: string;
+  description: string;
+  tamilVideo: string;
+  englishVideo: string;
+}
+
+const AboutCTA: React.FC<AboutCTAProps> = ({ title, description, tamilVideo, englishVideo }) => {
+  const [isTamil, setIsTamil] = useState(false);
+
   useEffect(() => {
-    // Animate h3 and p from the left
     gsap.fromTo(
       ".cta-text-h3",
-      {
-        opacity: 0,
-        x: -100, // Start from left
-      },
+      { opacity: 0, x: -100 },
       {
         opacity: 1,
         x: 0,
@@ -23,7 +26,7 @@ const AboutCTA = () => {
         ease: "easeOut",
         scrollTrigger: {
           trigger: ".cta-text-h3",
-          start: "top 100%", // Trigger when it's 80% from the top
+          start: "top 100%",
           toggleActions: "play none none none",
         },
       }
@@ -31,10 +34,7 @@ const AboutCTA = () => {
 
     gsap.fromTo(
       ".cta-text-p1",
-      {
-        opacity: 0,
-        x: -100, // Start from left
-      },
+      { opacity: 0, x: -100 },
       {
         opacity: 1,
         x: 0,
@@ -48,13 +48,9 @@ const AboutCTA = () => {
       }
     );
 
-    // Animate video from the right
     gsap.fromTo(
       ".cta-video",
-      {
-        opacity: 0,
-        x: 100, // Start from right
-      },
+      { opacity: 0, x: 100 },
       {
         opacity: 1,
         x: 0,
@@ -67,80 +63,42 @@ const AboutCTA = () => {
         },
       }
     );
-
-    // Animate h5 and p from the left
-    gsap.fromTo(
-      ".cta-text-h5",
-      {
-        opacity: 0,
-        x: -100, // Start from left
-      },
-      {
-        opacity: 1,
-        x: 0,
-        duration: 1,
-        ease: "easeOut",
-        scrollTrigger: {
-          trigger: ".cta-text-h5",
-          start: "top 80%",
-          toggleActions: "play none none none",
-        },
-      }
-    );
-
-    gsap.fromTo(
-      ".cta-text-p2",
-      {
-        opacity: 0,
-        x: -100, // Start from left
-      },
-      {
-        opacity: 1,
-        x: 0,
-        duration: 1,
-        ease: "easeOut",
-        scrollTrigger: {
-          trigger: ".cta-text-p2",
-          start: "top 80%",
-          toggleActions: "play none none none",
-        },
-      }
-    );
   }, []);
+
+  const buttonClass = (isSelected: boolean) =>
+    `px-6 py-3 font-medium text-sm shadow-lg border-[2px] border-teal-600 transition-all duration-300 transform ${isSelected
+      ? "bg-gradient-to-b from-teal-400 to-teal-800 text-white shadow-double-inset"
+      : "bg-gray-200 text-primary"
+    }`;
 
   return (
     <section className="px-8 py-16 lg:px-16 flex flex-col gap-4 bg-accent-2 text-primaryDark">
-      <h3 className="cta-text-h3 cta-text text-custom-20-bold lg:text-custom-32">
-        At Prepco, we get it—life is busy, and health can feel like an
-        afterthought. That’s where we come in.
-      </h3>
-      <p className="cta-text-p1 cta-text text-custom-14 lg:text-custom-20-2">
-        From fitness guides that keep you moving to wellness plans that work for
-        you, Prepco is your partner in living your healthiest, happiest life.
-      </p>
-      <button className="cta-button w-full p-3 lg:py-3 lg:px-6 bg-custom-gradient shadow-double-inset text-white lg:font-semibold text-custom-16-bold lg:text-lg custom-border-radius">
-        Book a consultation
-      </button>
-      <video
-        src={videos.hero}
-        className="cta-video inset-0 w-full h-full object-cover border-none rounded-3xl"
-        autoPlay
-        loop
-        muted
-        preload="auto"
-      >
-        <source src={videos.hero} type="video/mp4" />
+      <h3 className="cta-text-h3 text-custom-20-bold lg:text-custom-32">{title}</h3>
+      <p className="cta-text-p1 text-custom-14 lg:text-custom-20-2">{description}</p>
+      <div className="flex flex-row items-center justify-between">
+        <button className="cta-button w-fit p-3 lg:py-3 lg:px-6 bg-custom-gradient shadow-double-inset text-white lg:font-semibold text-custom-16-bold lg:text-lg custom-border-radius">
+          Book a consultation
+        </button>
+
+        <div className="flex">
+          <button
+            className={`${buttonClass(!isTamil)} rounded-l-3xl border-r-0`}
+            onClick={() => setIsTamil(false)}
+          >
+            English
+          </button>
+          <button
+            className={`${buttonClass(isTamil)} rounded-r-3xl border-l-0`}
+            onClick={() => setIsTamil(true)}
+          >
+            தமிழ்
+          </button>
+        </div>
+      </div>
+      <video key={isTamil ? tamilVideo : englishVideo} className="cta-video inset-0 w-full h-full object-cover border-none rounded-3xl" autoPlay loop muted preload="auto">
+        <source src={isTamil ? tamilVideo : englishVideo} type="video/mp4" />
         Your browser does not support the video tag.
       </video>
-      <div className="space-x-2 text-center px-2 lg:px-16">
-        <h5 className="cta-text-h5 cta-text text-custom-20-bold lg:text-custom-32">
-          so why wait
-        </h5>
-        <p className="cta-text-p2 cta-text text-custom-14 lg:text-custom-20">
-          Let Prepco show you how easy taking care of yourself can really be.
-          Because your health deserves the best—and so do you.
-        </p>
-      </div>
     </section>
   );
 };
