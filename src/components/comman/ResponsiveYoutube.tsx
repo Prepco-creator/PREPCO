1ba62998cd67a7c6f2f07646479a404882f940b1
Akram-00 <aliakram9789@gmail.com>
2025-01-25 23:24:48 +0530

    the youtube video is looped

--- src/components/comman/ResponsiveYoutube.tsx
+++ src/components/comman/ResponsiveYoutube.tsx
@@ -93,7 +93,7 @@ const ResponsiveYouTube: React.FC<ResponsiveYoutubeProps> = ({ videoURL }) => {
                                     ref={iframeRef}
                                     width={"100%"}
                                     height={height}
-                                    src={`https://www.youtube.com/embed/${videoId}?playlist=${videoId}&rel=0&modestbranding=1&autoplay=1&controls=1`}
+                                    src={`https://www.youtube.com/embed/${videoId}?playlist=${videoId}&rel=0&modestbranding=1&autoplay=1&controls=1&loop=1`}
                                     frameBorder="0"
                                     allowFullScreen
                                     allow="autoplay"
