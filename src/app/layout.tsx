a1fa1aa5f35a2082aed263b90bd2e500fbe2cae3
Akram-00 <aliakram9789@gmail.com>
2025-02-09 18:24:56 +0530

    microsoft clarity is added to prepco site

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import Navbar from "@/components/comman/Navbar";
 import LoadingAnimation from "@/components/loaders/LoadingAnimation";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "PREPCO | Where Care Meets Convenience",
@@ -16,6 +17,21 @@ export default function RootLayout({
 
   return (
     <html lang="en">
+      <head>
+        <Script
+          id="clarity-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "q74xa1mhvh");
+            `,
+          }}
+        />
+      </head>
       <body className="font-poppins">
         <LoadingAnimation />
         <Navbar />
