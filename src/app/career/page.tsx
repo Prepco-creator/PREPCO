1187025a68f669f9c77f6450ddf2b1beeebe8f8e
Akram-00 <aliakram9789@gmail.com>
2025-02-19 15:18:59 +0530

    blog routes created

---
+++ src/app/career/page.tsx
@@ -0,0 +1,10 @@
+import Career from '@/components/career/Career'
+import React from 'react'
+
+const page = () => {
+    return (
+        <Career />
+    )
+}
+
+export default page
---
+++ src/components/career/Career.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Banner from '../comman/Banner'
+
+const Career = () => {
+    return (
+        <section>
+            <Banner title='Career' />
+        </section>
+    )
+}
+
+export default Career
