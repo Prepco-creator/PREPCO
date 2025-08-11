abc8cf34ce780f736cbfeb9897ded9ba41759147
kannan-14-08-new <kannan14082003im@gmail.com>
2025-08-11 12:12:58 +0530

    add images and crop it

--- public/images/images.ts
+++ public/images/images.ts
@@ -1,251 +1,320 @@
 // GIF
-import drink from './gifs/drink.gif';
-import exercise from './gifs/exercise.gif';
-import lunchBox from './gifs/lunch-box.gif';
-import morning from './gifs/morning.gif';
-import sleep from './gifs/sleep.gif';
-import work from './gifs/work.gif';
-import englishBreakfast from './gifs/english-breakfast.gif';
-import family from './gifs/family.gif';
-import meditation from './gifs/meditation.gif';
-import reading from './gifs/reading.gif';
-import walk from './gifs/walk.gif';
-import hollowCircles from './gifs/hollow-circles.gif';
-import bulb from './gifs/bulb.gif';
-import tabletGuy from './gifs/tablet-guy.gif';
-import innovationBulb from './gifs/innovation-bulb.gif';
-import threeGuys from './gifs/three-guys.gif';
-import twoGuys from './gifs/two-guys.gif';
-import yogaUpDown from './gifs/yoga-up-down.gif';
-import runningWoman from './gifs/running-woman.gif';
-import lovingFamily from './gifs/loving-family.gif';
-import coupleWalking from './gifs/couple-walking.gif';
-import drinkWater from './gifs/drink-water.gif';
-import oneMemberPlan from './gifs/one-member.gif';
-import threeMemberPlan from './gifs/three-members-plan.gif';
+import drink from "./gifs/drink.gif";
+import exercise from "./gifs/exercise.gif";
+import lunchBox from "./gifs/lunch-box.gif";
+import morning from "./gifs/morning.gif";
+import sleep from "./gifs/sleep.gif";
+import work from "./gifs/work.gif";
+import englishBreakfast from "./gifs/english-breakfast.gif";
+import family from "./gifs/family.gif";
+import meditation from "./gifs/meditation.gif";
+import reading from "./gifs/reading.gif";
+import walk from "./gifs/walk.gif";
+import hollowCircles from "./gifs/hollow-circles.gif";
+import bulb from "./gifs/bulb.gif";
+import tabletGuy from "./gifs/tablet-guy.gif";
+import innovationBulb from "./gifs/innovation-bulb.gif";
+import threeGuys from "./gifs/three-guys.gif";
+import twoGuys from "./gifs/two-guys.gif";
+import yogaUpDown from "./gifs/yoga-up-down.gif";
+import runningWoman from "./gifs/running-woman.gif";
+import lovingFamily from "./gifs/loving-family.gif";
+import coupleWalking from "./gifs/couple-walking.gif";
+import drinkWater from "./gifs/drink-water.gif";
+import oneMemberPlan from "./gifs/one-member.gif";
+import threeMemberPlan from "./gifs/three-members-plan.gif";
 
 // SVG
-import logo from './svg/Logo.svg';
-import doubleQuote from './svg/Quotes.svg';
-import logoIcon from './svg/LogoIcon.svg';
-import peopleDuo from './svg/peopleduo.svg';
-import dummyHero from './svg/dummyHeroImage.svg';
-import LogoWhite from './svg/LogoWhite.svg';
-import whiteLogoIcon from './svg/whiteLogoIcon.svg';
-import xmark from './svg/xmark.svg';
-import doctorConsultation from './svg/doctor-consultaion.svg';
-import microscope from './svg/microscope.svg';
-import virtualConsultation from './svg/virtual-consultation.svg';
-import dentalInsurance from './svg/dental-inusrance.svg';
-import heartBeatCheck from './svg/heart-beat-check.svg';
-import offer from './svg/offer.svg';
-import dietian from './svg/dietian.svg';
-import medicineTablet from './svg/medicine-tablet.svg';
-import physicalTherapy from './svg/physical-therapy.svg';
-import zumbaSession from './svg/zumba-session.svg';
+import logo from "./svg/Logo.svg";
+import doubleQuote from "./svg/Quotes.svg";
+import logoIcon from "./svg/LogoIcon.svg";
+import peopleDuo from "./svg/peopleduo.svg";
+import dummyHero from "./svg/dummyHeroImage.svg";
+import LogoWhite from "./svg/LogoWhite.svg";
+import whiteLogoIcon from "./svg/whiteLogoIcon.svg";
+import xmark from "./svg/xmark.svg";
+import doctorConsultation from "./svg/doctor-consultaion.svg";
+import microscope from "./svg/microscope.svg";
+import virtualConsultation from "./svg/virtual-consultation.svg";
+import dentalInsurance from "./svg/dental-inusrance.svg";
+import heartBeatCheck from "./svg/heart-beat-check.svg";
+import offer from "./svg/offer.svg";
+import dietian from "./svg/dietian.svg";
+import medicineTablet from "./svg/medicine-tablet.svg";
+import physicalTherapy from "./svg/physical-therapy.svg";
+import zumbaSession from "./svg/zumba-session.svg";
 
 // JPG
-import testimonialImage from './jpgs/indian_testimonial_1.jpg';
-import testimonialImage2 from './jpgs/indian_testimonial_2.jpg';
+import testimonialImage from "./jpgs/indian_testimonial_1.jpg";
+import testimonialImage2 from "./jpgs/indian_testimonial_2.jpg";
 import minime from "./jpgs/minime.jpg";
 import ultimate from "./jpgs/Ultimate wellness.jpg";
 import complete from "./jpgs/Complete wellness (6).jpg";
 
-import Plan_V2_DiamondHealthPlan from './jpgs/Diamond Health Plan.jpg';
-import Plan_V2_GoldHealthPlan from './jpgs/Gold health plan.jpg';
-import Plan_V2_JustforTwoPLan from './jpgs/Just for two.jpg';
-import Plan_V2_QuadPlan from './jpgs/Quad plan.jpg';
-import Plan_V2_QuadPro from './jpgs/Quad pro.jpg'
+import Plan_V2_DiamondHealthPlan from "./jpgs/Diamond Health Plan.jpg";
+import Plan_V2_GoldHealthPlan from "./jpgs/Gold health plan.jpg";
+import Plan_V2_JustforTwoPLan from "./jpgs/Just for two.jpg";
+import Plan_V2_QuadPlan from "./jpgs/Quad plan.jpg";
+import Plan_V2_QuadPro from "./jpgs/Quad pro.jpg";
 
-import AboutUsBanner from './jpgs/AboutUsBanner.jpg'
+import AboutUsBanner from "./jpgs/AboutUsBanner.jpg";
 
-import PlansBanner1 from './jpgs/P-Website-01.jpg'
-import PlansBanner2 from './jpgs/P-Website-02.jpg'
-import PlansBanner3 from './jpgs/P-Website-03.jpg'
+import PlansBanner1 from "./jpgs/P-Website-01.jpg";
+import PlansBanner2 from "./jpgs/P-Website-02.jpg";
+import PlansBanner3 from "./jpgs/P-Website-03.jpg";
 
 // PNG
-import aboutHero from './pngs/about-hero-2.png';
-import walkingCouple from './pngs/walking-couple.png';
-import youtubeIcon from './pngs/youtube-icon.png';
-import whatsappIcon from './pngs/whatsapp-icon.png';
-import twitterIcon from './pngs/twitter-icon.png';
-import instagramIcon from './pngs/instagram-icon.png';
-import facebookIcon from './pngs/Facebook.png';
-import xIcon from './pngs/X.png';
-import threadsIcon from './pngs/Threads.png';
-import phoneIcon from './pngs/phone.png';
-import locationIcon from './pngs/location.png'
-import mailIcon from './pngs/mail.png';
-import popupImage from './pngs/popup-1.png';
-import pattern from './pngs/pattern-removebg-preview.png';
-import testimoneeProfile from './pngs/testimonee-profile.png'
-import paymentDoneTick from './pngs/payment-done-icon.png';
+import aboutHero from "./pngs/about-hero-2.png";
+import walkingCouple from "./pngs/walking-couple.png";
+import youtubeIcon from "./pngs/youtube-icon.png";
+import whatsappIcon from "./pngs/whatsapp-icon.png";
+import twitterIcon from "./pngs/twitter-icon.png";
+import instagramIcon from "./pngs/instagram-icon.png";
+import facebookIcon from "./pngs/Facebook.png";
+import xIcon from "./pngs/X.png";
+import threadsIcon from "./pngs/Threads.png";
+import phoneIcon from "./pngs/phone.png";
+import locationIcon from "./pngs/location.png";
+import mailIcon from "./pngs/mail.png";
+import popupImage from "./pngs/popup-1.png";
+import pattern from "./pngs/pattern-removebg-preview.png";
+import testimoneeProfile from "./pngs/testimonee-profile.png";
+import paymentDoneTick from "./pngs/payment-done-icon.png";
 
-import completeWellnessPlan from './pngs/complete-wellness.png';
-import just50 from './pngs/Just_50.png';
-import justForTwo from './pngs/just-for-two.png';
-import justForYou from './pngs/just-for-you.png';
-import miniMe from './pngs/mini-mi.png';
-import quadPlan from './pngs/Quad-plan.png';
-import quadPlanPro from './pngs/Quad-pro.png';
-import sheroPlan from './pngs/SHERO.png'
-import triMe from './pngs/Tri-me.png';
-import ultimateWellnessPlan from './pngs/ultimatewellness.png';
+import completeWellnessPlan from "./pngs/complete-wellness.png";
+import just50 from "./pngs/Just_50.png";
+import justForTwo from "./pngs/just-for-two.png";
+import justForYou from "./pngs/just-for-you.png";
+import miniMe from "./pngs/mini-mi.png";
+import quadPlan from "./pngs/Quad-plan.png";
+import quadPlanPro from "./pngs/Quad-pro.png";
+import sheroPlan from "./pngs/SHERO.png";
+import triMe from "./pngs/Tri-me.png";
+import ultimateWellnessPlan from "./pngs/ultimatewellness.png";
 
-import footerLogo from './pngs/footer-logo.png'
+import footerLogo from "./pngs/footer-logo.png";
 
-
-import pharmacy from './pngs/pharmacy.png'
-import dermatology from './pngs/dermatology.png'
-import nutritionist from './pngs/nutrionist.png'
-import yogaSessions from './pngs/yoga.png'
-import yogaTherapy from './pngs/yoga-therapy.png'
-import fitness from './pngs/fitness.png'
-import healthCare from './pngs/healthcare.png'
-import pestControl from './pngs/pest.png'
-import homeCare from './pngs/home.png'
-import homeHero2 from './pngs/family-image-2.png'
+import pharmacy from "./pngs/pharmacy.png";
+import dermatology from "./pngs/dermatology.png";
+import nutritionist from "./pngs/nutrionist.png";
+import yogaSessions from "./pngs/yoga.png";
+import yogaTherapy from "./pngs/yoga-therapy.png";
+import fitness from "./pngs/fitness.png";
+import healthCare from "./pngs/healthcare.png";
+import pestControl from "./pngs/pest.png";
+import homeCare from "./pngs/home.png";
+import homeHero2 from "./pngs/family-image-2.png";
 
 // WEBP
 
-
 import apolloHospital from "./partners-logos/apollo-hospital.png";
 import drArgawalsLogo from "./partners-logos/dr.-argawals-logo.png";
-import drAravindsIvfLogo from "./partners-logos/dr-aravind_s-ivf-logo.png";
-import hc9cIsr400x400 from "./partners-logos/hc9c-isr_400x400.jpg";
+import drAravindsIvfLogo from "./partners-logos/dr-aravind-s-ivf-logo.png";
+import hc9cIsr400x400 from "./partners-logos/hc9c-isr-400x400.jpg";
 import kaveriHospital from "./partners-logos/kaveri-hospital.png";
 import lifelineHospitalLogo from "./partners-logos/lifeline-hospital-logo.jpg";
 import medwayHospitalLogo from "./partners-logos/medway-hospital-logo.png";
 import mgmHospital from "./partners-logos/mgm-hospital.jpg";
-import simsLogoNew from "./partners-logos/sims-logo_new.png";
+import simsLogoNew from "./partners-logos/sims-logo-new.png";
 import sooriyaHospitalLogo from "./partners-logos/sooriya-hospital-logo.png";
 
+import homeHero from "./webp/family-image.webp";
 
-import homeHero from './webp/family-image.webp'
+// Hospital-images
+
+import bhaarathHospital from "./partners-logos/bhaarath-hospital.png";
+import chennaiNationalHospital from "./partners-logos/chennai-national-hospital.png";
+import chennaiOrthopedicCenter from "./partners-logos/chennai-orthopedic-center.png";
+import deltaHospital from "./partners-logos/delta-hospital.png";
+import dentistree from "./partners-logos/dentistree.png";
+import drArgawalsLogo2 from "./partners-logos/dr.-argawals-logo.png";
+import drAravindSIvfLogo from "./partners-logos/dr-aravind-s-ivf-logo.png";
+import faithHospital from "./partners-logos/faith-hospital.png";
+import girishwarihospital from "./partners-logos/girishwarihospital..png";
+import gkmHospital from "./partners-logos/gkm-hospital.png";
+import glenaeglesHospital from "./partners-logos/glenaegles-hospital.png";
+import hc9cIsr from "./partners-logos/hc9c-isr-400x400.jpg";
+import iswaryaHospital from "./partners-logos/iswarya-hospital.png";
+import kannappaMemorialHospital from "./partners-logos/kannappa-memorial-hospital-.png";
+import kaveriHospital2 from "./partners-logos/kaveri-hospital.png";
+import kedarHospital from "./partners-logos/kedar-hospital.png";
+import leelaEyeHospital from "./partners-logos/leela-eye-hospital.png";
+import lifelineHospital from "./partners-logos/lifeline-hospital-logo.jpg";
+import mahalakshmiHospitals from "./partners-logos/mahalakshmi-hospitals.png";
+import medwayHospitalLogo2 from "./partners-logos/medway-hospital-logo.png";
+import meridianLogo from "./partners-logos/meridian-logo.png";
+import mgmHospital2 from "./partners-logos/mgm-hospital.jpg";
+import onehealthHospital from "./partners-logos/onehealth-hospital.png";
+import rajuHospital from "./partners-logos/raju-hospital.png";
+import saanviHospital from "./partners-logos/saanvi-hospital.png";
+import shensHospitals from "./partners-logos/shens-hospitals.png";
+import simsLogoNew2 from "./partners-logos/sims-logo-new.png";
+import sivamHospital from "./partners-logos/sivam-hospital.png";
+import sooriyaHospitalLogo2 from "./partners-logos/sooriya-hospital-logo.png";
+import srinivasaHospital from "./partners-logos/srinivasa-hospital.png";
+import srmPrimeHospital from "./partners-logos/srm-prime-hospital.png";
+import venkateswaraHospital from "./partners-logos/venkateswara-hospital.png";
+import vijayaHospital from "./partners-logos/vijaya-hospital.png";
+import wcfHospital from "./partners-logos/wcf-hospital.png";
 
 const partnersLogo = {
-    apolloHospital,
-    drArgawalsLogo,
-    drAravindsIvfLogo,
-    hc9cIsr400x400,
-    kaveriHospital,
-    lifelineHospitalLogo,
-    medwayHospitalLogo,
-    mgmHospital,
-    simsLogoNew,
-    sooriyaHospitalLogo,
-}
+  apolloHospital,
+  drArgawalsLogo,
+  drAravindsIvfLogo,
+  hc9cIsr400x400,
+  kaveriHospital,
+  lifelineHospitalLogo,
+  medwayHospitalLogo,
+  mgmHospital,
+  simsLogoNew,
+  sooriyaHospitalLogo,
+};
 
 const gifs = {
-    drink,
-    exercise,
-    lunchBox,
-    morning,
-    sleep,
-    work,
-    englishBreakfast,
-    family,
-    meditation,
-    reading,
-    walk,
-    bulb,
-    hollowCircles,
-    tabletGuy,
-    twoGuys,
-    threeGuys,
-    innovationBulb,
-    yogaUpDown,
-    runningWoman,
-    lovingFamily,
-    coupleWalking,
-    drinkWater,
-    oneMemberPlan,
-    threeMemberPlan
+  drink,
+  exercise,
+  lunchBox,
+  morning,
+  sleep,
+  work,
+  englishBreakfast,
+  family,
+  meditation,
+  reading,
+  walk,
+  bulb,
+  hollowCircles,
+  tabletGuy,
+  twoGuys,
+  threeGuys,
+  innovationBulb,
+  yogaUpDown,
+  runningWoman,
+  lovingFamily,
+  coupleWalking,
+  drinkWater,
+  oneMemberPlan,
+  threeMemberPlan,
 };
 
 const svgs = {
-    logo,
-    doubleQuote,
-    logoIcon,
-    peopleDuo,
-    dummyHero,
-    LogoWhite,
-    whiteLogoIcon,
-    xmark,
-}
+  logo,
+  doubleQuote,
+  logoIcon,
+  peopleDuo,
+  dummyHero,
+  LogoWhite,
+  whiteLogoIcon,
+  xmark,
+};
 
 const pngs = {
-    paymentDoneTick,
-    completeWellnessPlan,
-    just50,
-    justForTwo,
-    justForYou,
-    miniMe,
-    quadPlan,
-    quadPlanPro,
-    sheroPlan,
-    triMe,
-    ultimateWellnessPlan,
-    homeCare,
-    aboutHero,
-    doctorConsultation,
-    microscope,
-    virtualConsultation,
-    dentalInsurance,
-    heartBeatCheck,
-    offer,
-    walkingCouple,
-    dietian,
-    medicineTablet,
-    physicalTherapy,
-    zumbaSession,
-    youtubeIcon,
-    whatsappIcon,
-    twitterIcon,
-    instagramIcon,
-    xIcon,
-    threadsIcon,
-    facebookIcon,
-    phoneIcon,
-    mailIcon,
-    popupImage,
-    pattern,
-    pharmacy,
-    dermatology,
-    nutritionist,
-    yogaSessions,
-    yogaTherapy,
-    fitness,
-    healthCare,
-    pestControl,
-    homeHero2,
-    testimoneeProfile,
-    locationIcon,
-    footerLogo,
-}
+  paymentDoneTick,
+  completeWellnessPlan,
+  just50,
+  justForTwo,
+  justForYou,
+  miniMe,
+  quadPlan,
+  quadPlanPro,
+  sheroPlan,
+  triMe,
+  ultimateWellnessPlan,
+  homeCare,
+  aboutHero,
+  doctorConsultation,
+  microscope,
+  virtualConsultation,
+  dentalInsurance,
+  heartBeatCheck,
+  offer,
+  walkingCouple,
+  dietian,
+  medicineTablet,
+  physicalTherapy,
+  zumbaSession,
+  youtubeIcon,
+  whatsappIcon,
+  twitterIcon,
+  instagramIcon,
+  xIcon,
+  threadsIcon,
+  facebookIcon,
+  phoneIcon,
+  mailIcon,
+  popupImage,
+  pattern,
+  pharmacy,
+  dermatology,
+  nutritionist,
+  yogaSessions,
+  yogaTherapy,
+  fitness,
+  healthCare,
+  pestControl,
+  homeHero2,
+  testimoneeProfile,
+  locationIcon,
+  footerLogo,
+};
 
 const webp = {
-    homeHero
-}
+  homeHero,
+};
 
 const jpgs = {
-    testimonialImage,
-    testimonialImage2,
-    ultimate,
-    complete,
+  testimonialImage,
+  testimonialImage2,
+  ultimate,
+  complete,
 
-    Plan_V2_DiamondHealthPlan,
-    Plan_V2_GoldHealthPlan,
-    Plan_V2_JustforTwoPLan,
-    Plan_V2_QuadPlan,
-    Plan_V2_QuadPro,
+  Plan_V2_DiamondHealthPlan,
+  Plan_V2_GoldHealthPlan,
+  Plan_V2_JustforTwoPLan,
+  Plan_V2_QuadPlan,
+  Plan_V2_QuadPro,
 
-    AboutUsBanner,
+  AboutUsBanner,
 
-    PlansBanner1,
-    PlansBanner2,
-    PlansBanner3,
-}
+  PlansBanner1,
+  PlansBanner2,
+  PlansBanner3,
+};
 
-
-export { gifs, svgs, pngs, jpgs, webp, partnersLogo };
+const hospitalLogos = {
+  bhaarathHospital,
+  chennaiNationalHospital,
+  chennaiOrthopedicCenter,
+  deltaHospital,
+  dentistree,
+  drArgawalsLogo2,
+  drAravindSIvfLogo,
+  faithHospital,
+  girishwarihospital,
+  gkmHospital,
+  glenaeglesHospital,
+  hc9cIsr,
+  iswaryaHospital,
+  kannappaMemorialHospital,
+  kaveriHospital2,
+  kedarHospital,
+  leelaEyeHospital,
+  lifelineHospital,
+  mahalakshmiHospitals,
+  medwayHospitalLogo2,
+  meridianLogo,
+  mgmHospital2,
+  onehealthHospital,
+  rajuHospital,
+  saanviHospital,
+  shensHospitals,
+  simsLogoNew2,
+  sivamHospital,
+  sooriyaHospitalLogo2,
+  srinivasaHospital,
+  srmPrimeHospital,
+  venkateswaraHospital,
+  vijayaHospital,
+  wcfHospital,
+};
+export { gifs, svgs, pngs, jpgs, webp, partnersLogo, hospitalLogos };
--- src/components/comman/TrustedParnters.tsx
+++ src/components/comman/TrustedParnters.tsx
@@ -1,46 +1,56 @@
-import React from 'react';
-import Image from 'next/image';
-import Marquee from 'react-fast-marquee';
-import { partnersLogo } from '@/data/trustedPartnersData';
+import React from "react";
+import Image from "next/image";
+import Marquee from "react-fast-marquee";
+import { partnersLogo } from "@/data/trustedPartnersData";
 
 const TrustedPartners = () => {
+  const midIndex = Math.ceil(partnersLogo.length / 2);
+  const firstHalf = partnersLogo.slice(0, midIndex);
+  const secondHalf = partnersLogo.slice(midIndex);
 
-    return (
-        <div className="w-full bg-gradient-to-br from-[#E8FDFD] via-[#D9FCFB] to-[#C0F8F7] py-6 space-y-4">
+  return (
+    <div className="w-full bg-gradient-to-br from-[#E8FDFD] via-[#D9FCFB] to-[#C0F8F7] py-6 space-y-4">
+      <h3 className="text-center text-custom-24 lg:text-custom-32 bg-custom-gradient bg-clip-text text-transparent">
+        Our Trusted – Top Line Hospitals{" "}
+      </h3>
 
-            <h3 className='text-center text-custom-24 lg:text-custom-32 bg-custom-gradient bg-clip-text text-transparent'>Our Trusted – Top Line Hospitals </h3>
+      <Marquee gradient={false} speed={30}>
+        {firstHalf.map((src, index) => (
+          <div
+            key={`top-${index}`}
+            className="mx-8 py-12 flex items-center justify-between min-w-[100px] h-[64px]"
+          >
+            <Image
+              src={src}
+              alt={`Partner ${index + 1}`}
+              width={120}
+              height={64}
+              quality={100}
+              className="object-contain h-16 w-auto"
+            />
+          </div>
+        ))}
+      </Marquee>
 
-            <Marquee gradient={false} speed={30}>
-                {partnersLogo.map((src, index) => (
-                    <div key={`top-${index}`} className="mx-8 py-12 flex items-center justify-between min-w-[100px] h-[64px]">
-                        <Image
-                            src={src}
-                            alt={`Partner ${index + 1}`}
-                            width={120}
-                            height={64}
-                            quality={100}
-                            className="object-contain h-16 w-auto"
-                        />
-                    </div>
-                ))}
-            </Marquee>
-
-            <Marquee gradient={false} speed={30} direction="right">
-                {partnersLogo.map((src, index) => (
-                    <div key={`bottom-${index}`} className="mx-8 py-12 flex items-center justify-between min-w-[100px] h-[64px]">
-                        <Image
-                            src={src}
-                            alt={`Partner ${1}`}
-                            width={120}
-                            height={64}
-                            quality={100}
-                            className="object-contain h-16 w-auto"
-                        />
-                    </div>
-                ))}
-            </Marquee>
-        </div>
-    );
+      <Marquee gradient={false} speed={30} direction="right">
+        {secondHalf.map((src, index) => (
+          <div
+            key={`bottom-${index}`}
+            className="mx-8 py-12 flex items-center justify-between min-w-[100px] h-[64px]"
+          >
+            <Image
+              src={src}
+              alt={`Partner ${1}`}
+              width={120}
+              height={64}
+              quality={100}
+              className="object-contain h-16 w-auto"
+            />
+          </div>
+        ))}
+      </Marquee>
+    </div>
+  );
 };
 
 export default TrustedPartners;
